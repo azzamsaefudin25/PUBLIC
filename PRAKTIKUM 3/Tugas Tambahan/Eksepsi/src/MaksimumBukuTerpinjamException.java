f7720342c6feebf11bcee6456bdd9cab2b9b03b7
Azzam Saefudin Rosyidi <126538984+azzamsaefudin25@users.noreply.github.com>
2024-03-08 18:09:04 +0700

    Delete PRAKTIKUM 3/Tugas Tambahan/Eksepsi/src/MaksimumBukuTerpinjamException.java

--- PRAKTIKUM 3/Tugas Tambahan/Eksepsi/src/MaksimumBukuTerpinjamException.java
+++
@@ -1,7 +0,0 @@
-public class MaksimumBukuTerpinjamException extends Exception{
-    // TODO 1: Buat konstructor dengan parameter message dan panggil konstruktor parent dengan keyword super
-    public MaksimumBukuTerpinjamException(String message){
-        super(message);
-    }
-}
-
